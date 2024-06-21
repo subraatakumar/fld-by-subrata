1933230255fe94bf486b1a9556b02661b4b808bf
subraatakumar <business.ttrc@gmail.com>
2024-06-21 12:09:55 +0530

    face recognised

--- public/script.js
+++ public/script.js
@@ -1,4 +1,7 @@
 const video = document.getElementById("video");
+const captureCanvas = document.getElementById("captureCanvas");
+const captureContext = captureCanvas.getContext("2d");
+let stream;
 
 Promise.all([
   faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
@@ -9,11 +12,9 @@ Promise.all([
 
 function startWebcam() {
   navigator.mediaDevices
-    .getUserMedia({
-      video: true,
-      audio: false,
-    })
-    .then((stream) => {
+    .getUserMedia({ video: true, audio: false })
+    .then((videoStream) => {
+      stream = videoStream;
       video.srcObject = stream;
     })
     .catch((error) => {
@@ -25,10 +26,11 @@ video.addEventListener("play", async () => {
   const canvas = faceapi.createCanvasFromMedia(video);
   document.body.append(canvas);
   faceapi.matchDimensions(canvas, { height: video.height, width: video.width });
+
   const labeledFaceDescriptors = await loadLabeledImages();
   const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6);
-  console.log("facematcher", faceMatcher);
-  setInterval(async () => {
+
+  const intervalId = setInterval(async () => {
     const detections = await faceapi
       .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
       .withFaceLandmarks()
@@ -42,26 +44,30 @@ video.addEventListener("play", async () => {
     canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
     //faceapi.draw.drawDetections(canvas, resizedDetections);
     //faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+
     const results = resizedDetections.map((d) =>
       faceMatcher.findBestMatch(d.descriptor)
     );
+
     if (results[0]) {
       console.log("result: ", results[0], results[0].label);
-      document.getElementById("name").innerText = results[0].label;
+      document.getElementById("name").innerText = "Name: " + results[0].label;
     } else {
-      document.getElementById("name").innerText = "";
+      document.getElementById("name").innerText = "Name: Unknown";
     }
-    //console.log("detections: ", detections);
-    await detectBlink(detections);
-  }, 100);
+
+    const live = await detectBlink(resizedDetections);
+    if (live) {
+      document.getElementById("name1").innerText = "Name: " + results[0].label;
+      captureImage();
+      stopVideo(intervalId);
+    }
+  }, 1000);
 });
 
-function loadLabeledImages() {
+async function loadLabeledImages() {
   const labels = [
-    {
-      name: "Subrata Kumar Das",
-      url: "./faces/Subrata Kumar Das/1.jpg",
-    },
+    { name: "Subrata Kumar Das", url: "./faces/Subrata Kumar Das/1.jpg" },
   ];
   return Promise.all(
     labels.map(async ({ name, url }) => {
@@ -79,7 +85,7 @@ function loadLabeledImages() {
 }
 
 async function detectBlink(detections) {
-  detections.forEach((detection) => {
+  for (const detection of detections) {
     const landmarks = detection.landmarks;
     const leftEye = landmarks.getLeftEye();
     const rightEye = landmarks.getRightEye();
@@ -92,10 +98,12 @@ async function detectBlink(detections) {
     if (ear < BLINK_THRESHOLD) {
       console.log("Blink detected");
       document.getElementById("liveness").innerText = "Liveness: Live";
+      return true;
     } else {
       document.getElementById("liveness").innerText = "Liveness: Not Live";
     }
-  });
+  }
+  return false;
 }
 
 function calculateEAR(eye) {
@@ -108,6 +116,28 @@ function calculateEAR(eye) {
   const C = Math.sqrt(
     Math.pow(eye[0].x - eye[3].x, 2) + Math.pow(eye[0].y - eye[3].y, 2)
   );
-  const ear = (A + B) / (2.0 * C);
-  return ear;
+  return (A + B) / (2.0 * C);
+}
+
+function captureImage() {
+  captureContext.drawImage(
+    video,
+    0,
+    0,
+    captureCanvas.width,
+    captureCanvas.height
+  );
+  const dataUrl = captureCanvas.toDataURL("image/png");
+  console.log("Captured Image URL:", dataUrl);
+  document.getElementById("image").src = dataUrl;
+  document.getElementById("beforeCapture").style.display = none;
+  document.getElementById("video").style.display = none;
+  document.getElementById("info").style.display = none;
+  // You can also send this data URL to your server for further processing
+}
+
+function stopVideo(intervalId) {
+  clearInterval(intervalId);
+  stream.getTracks().forEach((track) => track.stop());
+  console.log("Video stopped");
 }
--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 const video = document.getElementById("video");
+const captureCanvas = document.getElementById("captureCanvas");
+const captureContext = captureCanvas.getContext("2d");
+let stream;
 
 Promise.all([
   faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+  faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
   faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+  faceapi.nets.ssdMobilenetv1.loadFromUri("/models"),
 ]).then(startWebcam);
 
 function startWebcam() {
   navigator.mediaDevices
-    .getUserMedia({
-      video: true,
-      audio: false,
-    })
-    .then((stream) => {
+    .getUserMedia({ video: true, audio: false })
+    .then((videoStream) => {
+      stream = videoStream;
       video.srcObject = stream;
     })
     .catch((error) => {
@@ -23,12 +26,15 @@ video.addEventListener("play", async () => {
   const canvas = faceapi.createCanvasFromMedia(video);
   document.body.append(canvas);
   faceapi.matchDimensions(canvas, { height: video.height, width: video.width });
+
   const labeledFaceDescriptors = await loadLabeledImages();
   const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6);
-  setInterval(async () => {
+
+  const intervalId = setInterval(async () => {
     const detections = await faceapi
       .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
-      .withFaceLandmarks();
+      .withFaceLandmarks()
+      .withFaceDescriptors();
 
     const resizedDetections = faceapi.resizeResults(detections, {
       height: video.height,
@@ -37,21 +43,31 @@ video.addEventListener("play", async () => {
 
     canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
     //faceapi.draw.drawDetections(canvas, resizedDetections);
-    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+    //faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+
     const results = resizedDetections.map((d) =>
       faceMatcher.findBestMatch(d.descriptor)
     );
-    console.log("result: ", results[0]);
-    console.log("detections: ", detections);
-  }, 10000);
+
+    if (results[0]) {
+      console.log("result: ", results[0], results[0].label);
+      document.getElementById("name").innerText = "Name: " + results[0].label;
+    } else {
+      document.getElementById("name").innerText = "Name: Unknown";
+    }
+
+    const live = await detectBlink(resizedDetections);
+    if (live) {
+      document.getElementById("name1").innerText = "Name: " + results[0].label;
+      captureImage();
+      stopVideo(intervalId);
+    }
+  }, 1000);
 });
 
-function loadLabeledImages() {
+async function loadLabeledImages() {
   const labels = [
-    {
-      name: "Subrata Kumar Das",
-      url: "https://firebasestorage.googleapis.com/v0/b/fldbysubrata.appspot.com/o/user_images%2FSubrata%20Kumar%20Das%2F1.jpg?alt=media&token=8f726914-bc46-449f-89ae-318fd2d6d64a",
-    },
+    { name: "Subrata Kumar Das", url: "./faces/Subrata Kumar Das/1.jpg" },
   ];
   return Promise.all(
     labels.map(async ({ name, url }) => {
@@ -66,4 +82,62 @@ function loadLabeledImages() {
       return new faceapi.LabeledFaceDescriptors(name, descriptions);
     })
   );
+}
+
+async function detectBlink(detections) {
+  for (const detection of detections) {
+    const landmarks = detection.landmarks;
+    const leftEye = landmarks.getLeftEye();
+    const rightEye = landmarks.getRightEye();
+
+    const leftEAR = calculateEAR(leftEye);
+    const rightEAR = calculateEAR(rightEye);
+    const ear = (leftEAR + rightEAR) / 2;
+
+    const BLINK_THRESHOLD = 0.25;
+    if (ear < BLINK_THRESHOLD) {
+      console.log("Blink detected");
+      document.getElementById("liveness").innerText = "Liveness: Live";
+      return true;
+    } else {
+      document.getElementById("liveness").innerText = "Liveness: Not Live";
+    }
+  }
+  return false;
+}
+
+function calculateEAR(eye) {
+  const A = Math.sqrt(
+    Math.pow(eye[1].x - eye[5].x, 2) + Math.pow(eye[1].y - eye[5].y, 2)
+  );
+  const B = Math.sqrt(
+    Math.pow(eye[2].x - eye[4].x, 2) + Math.pow(eye[2].y - eye[4].y, 2)
+  );
+  const C = Math.sqrt(
+    Math.pow(eye[0].x - eye[3].x, 2) + Math.pow(eye[0].y - eye[3].y, 2)
+  );
+  return (A + B) / (2.0 * C);
+}
+
+function captureImage() {
+  captureContext.drawImage(
+    video,
+    0,
+    0,
+    captureCanvas.width,
+    captureCanvas.height
+  );
+  const dataUrl = captureCanvas.toDataURL("image/png");
+  console.log("Captured Image URL:", dataUrl);
+  document.getElementById("image").src = dataUrl;
+  document.getElementById("beforeCapture").style.display = none;
+  document.getElementById("video").style.display = none;
+  document.getElementById("info").style.display = none;
+  // You can also send this data URL to your server for further processing
+}
+
+function stopVideo(intervalId) {
+  clearInterval(intervalId);
+  stream.getTracks().forEach((track) => track.stop());
+  console.log("Video stopped");
 }
